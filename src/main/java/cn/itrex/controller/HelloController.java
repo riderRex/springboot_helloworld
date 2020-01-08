e85510efe7cfc0254cb09fc0c5fcd4fc9005edc4
Rex <44470267+riderRex@users.noreply.github.com>
2020-01-08 10:58:31 +0800

    Update HelloController.java

--- src/main/java/cn/itrex/controller/HelloController.java
+++ src/main/java/cn/itrex/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World777 !!";
+        return "Hello World888 !!";
     }
 }
