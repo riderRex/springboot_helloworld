99773216435bbf6326637de3fd0b24017d0ef7e9
heyanhua <15817033523@163.com>
2020-01-08 10:55:26 +0800

    springboot-helloworld777

--- src/main/java/cn/itrex/controller/HelloController.java
+++ src/main/java/cn/itrex/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World666 !!";
+        return "Hello World777 !!";
     }
 }
